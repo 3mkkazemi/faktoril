cad04d5eae660f03c6d6f71906d2a2f8d509a1d8
3mkkazemi <149534612+3mkkazemi@users.noreply.github.com>
2023-11-01 13:34:30 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //محمد مهدی محمد کاظمی
+            //تکلیف شماره5
+            //پیشرفته 1
+            Console.WriteLine("please enter your number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int Factoriel = 1;
+            for(int i=1;i<=number;i++)
+            {
+                Factoriel *= i;
+            }
+            Console.WriteLine(Factoriel);
+            Console.ReadKey();
+
+        }
+    }
+}
